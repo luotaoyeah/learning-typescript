8cc4ef16b0d6daad6f0ac997cc82c2e0110fb42b
luotao <luotaoyeah@gmail.com>
2018-03-21 22:03:20 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,41 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const path = require('path');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require("path");
 
 module.exports = [
   {
-    entry: path.resolve(__dirname + '/src/index.ts'),
+    entry: path.resolve(__dirname + "/src/index.ts"),
     output: {
-      path: path.resolve(__dirname + '/dist/'),
-      filename: 'index.js',
-      libraryTarget: 'umd',
-      library: 'ts-test',
+      path: path.resolve(__dirname + "/dist/"),
+      filename: "index.js",
+      libraryTarget: "umd",
+      library: "ts-test",
       umdNamedDefine: true
     },
     module: {
       rules: [
         {
-          test: /\.ts$/,
-          loader: 'ts-loader',
+          test: /\.tsx?$/,
+          loader: "tslint-loader",
+          enforce: "pre",
+          include: [path.resolve("src")]
+        },
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
           exclude: /node_modules/
         }
       ]
     },
     resolve: {
-      extensions: ['.ts', '.js']
+      extensions: [".ts", ".js"]
     },
-    devtool: 'source-map',
-    plugins: []
+    devtool: "source-map",
+    plugins: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          ecma: 7
+        }
+      })
+    ]
   }
 ];
