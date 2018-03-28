152510e36cfd3cac40e79530be512e7b012f538c
luotao <luotaoyeah@gmail.com>
2018-03-28 22:03:07 +0800

    ts2.8: conditional types: distributive conditional types

--- src/ts2.8/conditional-types.ts
+++ src/ts2.8/conditional-types.ts
@@ -1,10 +1,11 @@
 /**
- * Const-named properties
+ * https://github.com/Microsoft/TypeScript/wiki/What's-new-in-TypeScript#conditional-types
+ * --------------------------------------------------
  */
 
-console.log("----------");
-
 /*
+ * T extends U ? X : Y;
+ * 条件类型表示的是一个类型：如果 T 可以赋值给 U，则类型为 X，否则类型为 Y；
  * --------------------------------------------------
  */
 
@@ -24,6 +25,33 @@ let foo: Foo = "tom";
   /!* TS2322: Type '18' is not assignable to type 'string'. *!/
   let bar: Foo = 18;
 */
+
+/*
+ * 获取以'类型名称'为字面量的类型；
+ * --------------------------------------------------
+ */
+
+type TypeName<T> = T extends string
+  ? "string"
+  : T extends number
+    ? "number"
+    : T extends boolean
+      ? "boolean"
+      : T extends undefined
+        ? "undefined"
+        : T extends Function ? "function" : "object";
+
+type T0 = TypeName<"tom">;
+type T1 = TypeName<18>;
+type T2 = TypeName<true>;
+type T3 = TypeName<() => void>;
+type T4 = TypeName<[1]>;
+
+const t0: T0 = "string";
+const t1: T1 = "number";
+const t2: T2 = "boolean";
+const t3: T3 = "function";
+const t4: T4 = "object";
 
 /*
  * --------------------------------------------------
@@ -50,11 +78,13 @@ declare function createLabel<T extends number | string>(
   idOrName: T
 ): IdOrName<T>;
 
-let a = createLabel("tom");
-let b = createLabel(18);
+/*
+  let a = createLabel("tom");
+  let b = createLabel(18);
+*/
 
 /*
- * get the type of element of an array
+ * 获取数组的元素类型
  * --------------------------------------------------
  */
 
@@ -69,7 +99,7 @@ type ElementType = Flatten02<typeof array>;
 const element: ElementType = 18;
 
 /*
- * get the return type of a function
+ * 获取函数的返回类型
  * --------------------------------------------------
  */
 
@@ -84,17 +114,61 @@ type ReturnType = ReturnTypeOf<typeof f01>;
 const x: ReturnType = "cat";
 
 /*
- * type being distributed
+ * 分发的条件类型；
+ * 如果条件类型中被检查的类型（T）仅仅是一个类型参数，则这个条件类型称为'分发的条件类型'；
  * --------------------------------------------------
  */
-
-type DistributeType<T> = T extends any ? T[] : T;
-
-type DistributedType = DistributeType<string | number>;
-
+console.log("----------: distributive conditional types");
+type DistributiveType<T> = T extends any ? T[] : T;
+type DistributedType = DistributiveType<string | number>;
 /* string[] | number[] */
 let array03: DistributedType = ["a", "b"];
 array03 = [1, 2];
 
-console.log("----------");
+console.log("----------: distributive conditional types");
+type BoxedValue<T> = { value: T };
+type BoxedArray<T> = { array: T[] };
+type Boxed<T> = T extends any[] ? BoxedArray<T[number]> : BoxedValue<T>;
+
+type T22 = Boxed<string | number[]>;
+let t22: T22 = { value: "tom" };
+t22 = { array: [1, 2, 3] };
+
+/*
+ * 分发的条件类型可以用来过滤合并类型（union type）；
+ * --------------------------------------------------
+ */
+console.log("----------: filter union types");
+type Diff<T, U> = T extends U ? never : T;
+type Filter<T, U> = T extends U ? T : never;
+/* t23 的类型为 string */
+let t23: Diff<string | null | undefined, null | undefined> = "tom";
+
+/*
+ * 条件类型可以跟映射类型（mapped type）结合；
+ * --------------------------------------------------
+ */
+console.log("----------: combine with mapped types");
+type FunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? K : never
+}[keyof T];
+type NonFunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? never : K
+}[keyof T];
+
+interface I24 {
+  id: number;
+  name: string;
+  run(): void;
+  sleep(): void;
+}
+
+type T24 = FunctionPropertyNames<I24>;
+let t24: T24 = "run";
+t24 = "sleep";
+
+type T25 = NonFunctionPropertyNames<I24>;
+let t25: T25 = "id";
+t25 = "name";
+
 export {};
