e50946c52c697ee85e41735f1ddd75fd5d1efabc
luotao <luotaoyeah@gmail.com>
2018-03-29 16:27:14 +0800

    ts2.8: conditional types

--- src/ts2.8/conditional-types.ts
+++ src/ts2.8/conditional-types.ts
@@ -1,5 +1,5 @@
 /**
- * https://github.com/Microsoft/TypeScript/wiki/What's-new-in-TypeScript#conditional-types
+ * https://github.com/Microsoft/TypeScript/wiki/What%27s-new-in-TypeScript#conditional-types
  * --------------------------------------------------
  */
 
@@ -99,21 +99,6 @@ type ElementType = Flatten02<typeof array>;
 const element: ElementType = 18;
 
 /*
- * 获取函数的返回类型
- * --------------------------------------------------
- */
-
-type ReturnTypeOf<T> = T extends ((...args: any[]) => infer R) ? R : T;
-
-function f01(): string {
-  return "tom";
-}
-
-type ReturnType = ReturnTypeOf<typeof f01>;
-
-const x: ReturnType = "cat";
-
-/*
  * 分发的条件类型；
  * 如果条件类型中被检查的类型（T）仅仅是一个类型参数，则这个条件类型称为'分发的条件类型'；
  * --------------------------------------------------
@@ -159,16 +144,129 @@ type NonFunctionPropertyNames<T> = {
 interface I24 {
   id: number;
   name: string;
+
   run(): void;
+
   sleep(): void;
 }
 
+/* "run" | "sleep" */
 type T24 = FunctionPropertyNames<I24>;
 let t24: T24 = "run";
 t24 = "sleep";
 
+/* "id" | "name" */
 type T25 = NonFunctionPropertyNames<I24>;
 let t25: T25 = "id";
 t25 = "name";
+
+/*
+ * 条件类型可以跟映射类型（mapped type）结合；
+ * --------------------------------------------------
+ */
+console.log("----------: not allowed to reference themselves");
+/*
+  /!* TS2456: Type alias 'T26' circularly references itself. *!/
+  type T26<T> = T extends any[] ? T26<T[number]> : never;
+*/
+
+/*
+ * 条件类型中的类型推断；
+ * 在条件类型的 extends 子句中可以使用关键字 infer 引入类型变量，
+ * 引入的类型变量可以在 true 分支中使用；
+ * --------------------------------------------------
+ */
+console.log("----------: type inference in conditional types");
+type ReturnType<T> = T extends (...args: any[]) => infer R ? R : any;
+/* T27 的实际类型为 string */
+type T27 = ReturnType<() => string>;
+let t27: T27 = "tom";
+
+/*
+ * infer 可以嵌套使用；
+ * --------------------------------------------------
+ */
+console.log("----------: nested infer");
+type Unpack<T> = T extends (infer U)[]
+  ? U
+  : T extends (...args: any[]) => infer U
+    ? U
+    : T extends Promise<infer U> ? U : T;
+
+/* string */
+type T28 = Unpack<string>;
+/* string */
+type T29 = Unpack<string[]>;
+/* string */
+type T30 = Unpack<Promise<string>>;
+/* string */
+type T31 = Unpack<() => string>;
+/* string */
+type T32 = Unpack<Unpack<Promise<string[]>>>;
+let t32: T32 = "tom";
+
+/*
+ * 同一个类型变量可以出现在多个不同的地方；
+ * 如果出现的位置是协变（co-variant）的关系，则会返回合并类型（union type）；
+ * 如果出现的位置时逆变（contra-variant）的关系，则会返回交叉类型（intersection type）；
+ * --------------------------------------------------
+ */
+console.log("----------: multiple locations for the same type variable");
+
+type Foo02<T> = T extends { a: infer U; b: infer U } ? U : never;
+/* string | number */
+type T33 = Foo02<{ a: string; b: number }>;
+let t33: T33 = "tom";
+t33 = 18;
+
+type Bar02<T> = T extends { a: (x: infer U) => void; b: (x: infer U) => void }
+  ? U
+  : never;
+
+/* {name: string} & {age: number} */
+type T34 = Bar02<{
+  a: (x: { name: string }) => void;
+  b: (x: { age: number }) => void;
+}>;
+let t34: T34 = { name: "tom", age: 18 };
+
+/*
+ * 当推断一个重载函数的类型的时候，会从最后一个函数签名进行推断；
+ * 因为理论上来讲，最后一个签名是最宽容的，包含了所有的情况；
+ * --------------------------------------------------
+ */
+console.log("----------: infer type of overloaded function");
+
+declare function foo02(x: string): string;
+declare function foo02(x: number): number;
+declare function foo02(x: string | number): string | number;
+
+/* string | number */
+type T35 = ReturnType<typeof foo02>;
+let t35: T35 = "tom";
+t35 = 18;
+
+/*
+ * 在普通的类型参数约束中，不能使用 infer 关键字进行类型推断；
+ * --------------------------------------------------
+ */
+console.log(
+  "----------: can't use infer in regular type parameter constraint clause"
+);
+/*
+  /!* TS1338: 'infer' declarations are only permitted in the 'extends' clause of a conditional type. *!/
+  type ReturnType02<T extends (...args: any[]) => infer R> = R;
+*/
+
+/*
+ * 在 typescript@2.8 中提供了 5 个预定义的条件类型；
+ * Exclude<T, U> ：移除 T 中可以赋值给 U 的类型；
+ * Extract<T, U> ：保留 T 中可以赋值给 U 的类型；
+ * NonNullable<T> ：移除 T 中的 null 和 undefined；
+ * ReturnType<T> ：获取函数 T 的返回类型；
+ * InstanceType<T> ：获取构造方法 T 的实例类型；
+ * --------------------------------------------------
+ */
+console.log("----------: predefined conditional types");
 
 export {};
