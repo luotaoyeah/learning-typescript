3492239532d14d929e1ca785c5660424afedf7eb
luotao <luotaoyeah@gmail.com>
2018-06-26 17:38:29 +0800

    handbook: basic types: any: represent union type

---
+++ src/handbook/01-basic-types/01-08/01-08-C.ts
@@ -0,0 +1,10 @@
+/*
+ * any 类型可以用来表示 union type；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+const array: any = [9, "foo", true];
+/* [ 9, 'foo', true ] */
+console.log(array);
+
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./handbook/01-basic-types/01-08/01-08-B";
+import "./handbook/01-basic-types/01-08/01-08-C";
