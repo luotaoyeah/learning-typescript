a22154176e7af202663178119287b017dd0ef66c
luotao <luotaoyeah@gmail.com>
2018-06-28 11:05:21 +0800

    handbook: variable declaration: variable capture

---
+++ src/handbook/02/02-01/02-01-C.ts
@@ -0,0 +1,47 @@
+/*
+ * Variable capturing quirks
+ */
+
+(function() {
+  /*
+   *
+   */
+  console.log("\n-------------------------------------------------- 01");
+
+  /*
+   * setTimeout() 会在循环执行完之后再执行，此时 i = 10；
+   * 由于 i 是使用 var 定义的，setTimeout 中的所有 function 访问的都是同一个 i（10）；
+   */
+  (function() {
+    for (var i = 0; i < 10; i++) {
+      setTimeout(function() {
+        console.log(i);
+      }, i * 1000);
+    }
+  })();
+
+  /*
+   * 解决方法01：使用 IIFE（Immediately Invoked Function Expression）；
+   */
+  (function() {
+    for (var i = 0; i < 10; i++) {
+      (function(i) {
+        setTimeout(function() {
+          console.log(i);
+        }, 1000 * i);
+      })(i);
+    }
+  })();
+
+  /*
+   * 解决方法02：使用 let 替换 var；
+   * let 声明的变量为 block scope，每次循环都会捕获本次循环的 i 的值；
+   */
+  (function() {
+    for (let i = 0; i < 10; i++) {
+      setTimeout(function() {
+        console.log(i);
+      }, 1000 * i);
+    }
+  })();
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./handbook/02/02-01/02-01-B.ts";
+import "./handbook/02/02-01/02-01-C.ts";
