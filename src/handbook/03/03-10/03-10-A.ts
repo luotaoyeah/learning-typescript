1a14dfceab52bc3a7ba7362d8d77aa5b6318676c
luotao <luotaoyeah@gmail.com>
2018-07-19 21:54:41 +0800

    handbook: interface: interface extends class

---
+++ src/handbook/03/03-10/03-10-A.ts
@@ -0,0 +1,28 @@
+/*
+ * Interface Extends Classes
+ */
+
+/*
+ * interface 不仅可以 extends 其他的 interface，
+ * 还可以 extends 其他的 class；
+ * 此时，interface 继承了该 class 所有的属性，
+ * 但是没有继承这些属性的实现；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+class Point {
+  x: string;
+  y?: string;
+}
+
+interface IPoint extends Point {
+  z: string;
+}
+
+/*
+ * 继承的 y 属性依然是一个 optional 属性；
+ */
+const point: IPoint = { x: "x", z: "z" };
+console.log(point);
+
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./handbook/03/03-09/03-09-A.ts";
+import "./handbook/03/03-10/03-10-A.ts";
