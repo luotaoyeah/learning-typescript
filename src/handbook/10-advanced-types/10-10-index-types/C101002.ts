331a3b70b44e34b1f505d1bbb3bce3ed9c7e06cb
luotao <luotaoyeah@gmail.com>
2018-12-18 21:33:57 +0800

    handbook: advanced type: index type: T[K]

---
+++ src/handbook/10-advanced-types/10-10-index-types/C101002.ts
@@ -0,0 +1,34 @@
+/*
+ * Advanced Types
+ *     Index types
+ *         T[K]
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * T[K] 称之为 indexed access operator
+   * 即对于类型 T 和 K，
+   * T[K] 表示 T 的 K 属性的属性值的类型，
+   * 其中 K 是一个 string literal 类型
+   */
+
+  interface Person {
+    name: string;
+    age: number;
+  }
+
+  /*
+   * T01 的实际类型为 Person 的 name 属性的类型，即 string
+   */
+  type T01 = Person["name"];
+  const t01: T01 = "FOO";
+
+  /*
+   * T02 的实际类型为 Person 的 age 属性的类型，即 number
+   */
+  type T02 = Person["age"];
+  const t02: T02 = 9;
+}
+
+export {};
