42cf46eba99e9ba997845409bcf135417e6ec8ce
luotao <luotaoyeah@gmail.com>
2018-12-22 10:26:56 +0800

    handbook: advanced type: mapped type: Record<K, T>

---
+++ src/handbook/10-advanced-types/10-11-mapped-types/C101109.ts
@@ -0,0 +1,20 @@
+/*
+ * Advanced Types
+ *     Mapped types
+ *         Recored<K, T>
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * Recored<K, T>
+   * 根据一组属性名称（K）和它们的类型（T），创建一个新的类型
+   * 即，所有这些属性的类型都是一样的
+   */
+
+  type F = Record<"foo" | "bar", boolean>;
+
+  const f: F = { foo: true, bar: false };
+}
+
+export {};
