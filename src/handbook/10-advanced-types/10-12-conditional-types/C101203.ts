fc9505f1cf45d88450fe373802d5fb13aaa25019
luotao <luotaoyeah@gmail.com>
2018-12-22 15:19:50 +0800

    handbook: advanced type: conditional type: defer

--- src/handbook/10-advanced-types/10-12-conditional-types/C101203.ts
+++ src/handbook/10-advanced-types/10-12-conditional-types/C101203.ts
@@ -15,18 +15,19 @@ console.log("\n-------------------------------------------------- 01");
     return t as any;
   }
 
+  /*
+   * f() 函数的返回类型为：T extends Foo ? string : number
+   * 此时 T 的实际类型为 U，所以返回类型为：U extends Foo ? string : number
+   * 因为条件类型中的 T 包含了其他的类型参数 U，因此结果类型会被推迟，
+   * 因此，此时的结果类型为分支类型的联合类型：string | number
+   */
   function foo<U>(u: U) {
-    /*
-     * f() 函数的返回类型为：T extends Foo ? string : number
-     * 此时 T 的实际类型为 U，所以返回类型为：U extends Foo ? string : number
-     * 因为条件类型中的 T 包含了其他的类型参数 U，因此结果类型会被推迟，
-     * 因此，此时的结果类型为分支类型的联合类型：string | number
-     */
-
     // TS2322: Type 'U extends Foo ? string : number' is not assignable to type 'string'.
     //   Type 'string | number' is not assignable to type 'string'.
     //     Type 'number' is not assignable to type 'string'.
-    let y: string = f(u);
+    /*
+        let y: string = f(u);
+    */
   }
 }
 
