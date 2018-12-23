235d3f409dd4216190508da35d5ae13ef2a770d0
luotao <luotaoyeah@gmail.com>
2018-12-23 16:43:10 +0800

    handbook: advanced type: conditional type: Exclude<T, U>

---
+++ src/handbook/10-advanced-types/10-12-conditional-types/C101214.ts
@@ -0,0 +1,27 @@
+/*
+ * Advanced Types
+ *     Conditional Types
+ *         Predefined conditional types
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * TS 标准库中预定义了一些常用的条件类型：
+   *     Exclude<T, U> 从 T 中排除可以赋值给 U 的类型
+   */
+
+  type T01 = Exclude<string | null | undefined, null | undefined>;
+  // @ts-ignore
+  const t01: T01 = null; // TS2322: Type 'null' is not assignable to type 'string'.
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  type MyExclude<T, U> = T extends U ? never : T;
+  type T01 = MyExclude<string | null | undefined, null | undefined>;
+  // @ts-ignore
+  const t01: T01 = null; // TS2322: Type 'null' is not assignable to type 'string'.
+}
+
+export {};
