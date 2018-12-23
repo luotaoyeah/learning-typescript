6bf65020c7175753dfe7f50bfbfc5e84d67ab0fe
luotao <luotaoyeah@gmail.com>
2018-12-23 16:45:59 +0800

    handbook: advanced type: conditional type: Extract<T, U>

--- src/handbook/10-advanced-types/10-12-conditional-types/C101215.ts
+++ src/handbook/10-advanced-types/10-12-conditional-types/C101215.ts
@@ -2,25 +2,25 @@
  * Advanced Types
  *     Conditional Types
  *         Predefined conditional types
- *             Exclude<T, U>
+ *             Extract<T, U>
  */
 
 console.log("\n-------------------------------------------------- 01");
 {
   /*
-   * TS 标准库中预定义了一些常用的条件类型：
-   *     Exclude<T, U> 从 T 中排除可以赋值给 U 的类型
+   * Extract<T, U>
+   * 从 T 中提取可以赋值给 U 的类型
    */
 
-  type T01 = Exclude<string | null | undefined, null | undefined>;
+  type T01 = Extract<string | null | undefined, string>;
   // @ts-ignore
   const t01: T01 = null; // TS2322: Type 'null' is not assignable to type 'string'.
 }
 
 console.log("\n-------------------------------------------------- 02");
 {
-  type MyExclude<T, U> = T extends U ? never : T;
-  type T01 = MyExclude<string | null | undefined, null | undefined>;
+  type MyExtract<T, U> = T extends U ? T : never;
+  type T01 = MyExtract<string | null | undefined, string>;
   // @ts-ignore
   const t01: T01 = null; // TS2322: Type 'null' is not assignable to type 'string'.
 }
