0d595c4f4bbbea33d3709c9155c765c785e8dd41
luotao <luotaoyeah@gmail.com>
2018-12-23 15:39:11 +0800

    handbook: advanced type: conditional type: infer

---
+++ src/handbook/10-advanced-types/10-12-conditional-types/C101210.ts
@@ -0,0 +1,43 @@
+/*
+ * Advanced Types
+ *     Conditional Types
+ *         Type inference in conditional types
+ *             infer
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 可以在不同的地方，使用相同的类型变量名称
+   */
+
+  type UnpackType<T> = T extends () => infer U
+    ? U
+    : T extends Array<infer U>
+    ? U
+    : T extends Promise<infer U>
+    ? U
+    : T;
+
+  function fn01(): number {
+    return 9;
+  }
+
+  type T01 = UnpackType<typeof fn01>;
+  // @ts-ignore
+  const t01: T01 = "0"; // TS2322: Type '"0"' is not assignable to type 'number'.
+
+  type T02 = UnpackType<Array<boolean>>;
+  // @ts-ignore
+  const t02: T02 = 0; // TS2322: Type '0' is not assignable to type 'boolean'.
+
+  type T03 = UnpackType<Promise<string>>;
+  // @ts-ignore
+  const t03: T03 = 0; // TS2322: Type '0' is not assignable to type 'string'.
+
+  type T04 = UnpackType<undefined>;
+  // @ts-ignore
+  const t04: T04 = 0; // TS2322: Type '0' is not assignable to type 'undefined'.
+}
+
+export {};
