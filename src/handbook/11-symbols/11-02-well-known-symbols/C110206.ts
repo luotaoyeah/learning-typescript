daacd1fcce9828926db40d048b1378ac2631d4db
luotao <luotaoyeah@gmail.com>
2018-12-25 18:56:25 +0800

    handbook: symbol: Symbol.toPrimitive

---
+++ src/handbook/11-symbols/11-02-well-known-symbols/C110206.ts
@@ -0,0 +1,56 @@
+/*
+ * Symbols
+ *     Well-known Symbols
+ *         Symbol.toPrimitive
+ *
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * [Symbol.toPrimitive] 方法，
+   * 当需要将对象隐式转换为：string，number，default 时，会调用该方法
+   */
+
+  const obj = {
+    [Symbol.toPrimitive](hint: "string" | "number" | "default") {
+      if (hint === "string") {
+        return "STRING";
+      }
+
+      if (hint === "number") {
+        return 99;
+      }
+
+      return "DEFAULT";
+    }
+  };
+
+  console.log(`${obj}`); // STRING
+  console.log(+obj); // 99
+  console.log(obj + ""); // DEFAULT
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * 在 ES2015 之前，如果需要进行隐式类型转换，
+   * 如果需要转换为 string，则首先调用 toString() 方法，如果没有则调用 valueOf() 方法，
+   * 如果需要转换为 number，则首先调用 valueOf() 方法，如果没有则调用 toString() 方法
+   */
+
+  const obj = {
+    valueOf() {
+      return 99;
+    },
+    toString() {
+      return "STRING";
+    }
+  };
+
+  console.log(`${obj}`); // STRING
+  console.log(+obj); // 99
+  console.log(obj + ""); // 99
+}
+
+export {};
