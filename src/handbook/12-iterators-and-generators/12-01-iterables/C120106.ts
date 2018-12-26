54304b4e6218ffa3b55c5c477023b5b8d3c740e4
luotao <luotaoyeah@gmail.com>
2018-12-26 21:25:04 +0800

    handbook: iterable: for...in

---
+++ src/handbook/12-iterators-and-generators/12-01-iterables/C120106.ts
@@ -0,0 +1,43 @@
+/*
+ * Iterators and Generators
+ *     Iterables
+ *         for...in
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * for...in 会遍历继承的属性
+   */
+
+  const foo = { x: "x" };
+  const bar = { y: "y" };
+  Object.setPrototypeOf(bar, foo);
+
+  console.log(Object.getOwnPropertyNames(bar)); // [ 'y' ]
+  for (const i in bar) {
+    // y
+    // x
+    console.log(i);
+  }
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * 通常需要在 for...in 循环中，通过 Object.prototype.hasOwnProperty() 方法，
+   * 排除继承的属性，只遍历自有属性
+   */
+
+  const foo = { x: "x" };
+  const bar = { y: "y" };
+  Object.setPrototypeOf(bar, foo);
+
+  for (const i in bar) {
+    if (bar.hasOwnProperty(i)) {
+      console.log(i); // y
+    }
+  }
+}
+
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./handbook/12-iterators-and-generators/12-01-iterables/C120105";
+import "./handbook/12-iterators-and-generators/12-01-iterables/C120106";
