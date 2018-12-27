972b3a584d30640e8d89fc15b4b50000099f25a7
luotao <luotaoyeah@gmail.com>
2018-12-27 18:04:39 +0800

    handbook: module: import: import a single export from a module

---
+++ src/handbook/13-modules/13-03-import/13-03-01/C13030101.ts
@@ -0,0 +1,20 @@
+/*
+ * Modules
+ *     Import
+ *         Import a single export from a module
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * 使用 import...from 语句，从某个模块中引入数据
+ */
+import { name } from "../../13-02-export/13-02-01/C13020101";
+console.log(name); // FOO
+
+console.log("\n-------------------------------------------------- 02");
+/*
+ * 在引入的时候，可以使用 as 给引入的数据重新取一个名字，
+ * 这个特性可以避免命名冲突
+ */
+import { name as NAME } from "../../13-02-export/13-02-01/C13020101";
+console.log(NAME); // FOO
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./handbook/13-modules/13-02-export/13-02-03/C13020301";
+import "./handbook/13-modules/13-03-import/13-03-01/C13030101";
