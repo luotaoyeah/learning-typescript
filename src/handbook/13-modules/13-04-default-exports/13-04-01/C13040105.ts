54a95f62cb00ff203ebc1829c6b2413c43fbed37
luotao <luotaoyeah@gmail.com>
2018-12-28 21:12:11 +0800

    handbook: module: default export: default export a variable

---
+++ src/handbook/13-modules/13-04-default-exports/13-04-01/C13040105.ts
@@ -0,0 +1,14 @@
+/*
+ * Modules
+ *     Default exports
+ *        default export a variable
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * 只有类，函数，接口的声明，可以直接指定为默认输出，
+ * 对于变量声明，需要先声明变量，再指定为默认输出
+ */
+
+const name: string = "BAR";
+export default name;
