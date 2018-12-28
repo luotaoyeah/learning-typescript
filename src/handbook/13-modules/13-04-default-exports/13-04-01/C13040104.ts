20de3999d16ab231e4a636073489fddf1233d94e
luotao <luotaoyeah@gmail.com>
2018-12-28 21:09:30 +0800

    handbook: module: default export: optional name

---
+++ src/handbook/13-modules/13-04-default-exports/13-04-01/C13040104.ts
@@ -0,0 +1,28 @@
+/*
+ * Modules
+ *     Default exports
+ *        optional name
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * 当直接默认输出一个函数（接口，类）声明时，名称可以省略
+ */
+
+/*
+  export default function() {
+    console.log("FOO");
+  }
+*/
+
+/*
+  export default class {
+    constructor(public name: string) {}
+  }
+*/
+
+/*
+  export default interface P {
+    name: string;
+  }
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./handbook/13-modules/13-04-default-exports/13-04-01/C13040102";
+import "./handbook/13-modules/13-04-default-exports/13-04-01/C13040105";
