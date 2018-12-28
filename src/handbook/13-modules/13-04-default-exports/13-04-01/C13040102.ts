eeeedfede607e365101e86e1432b18c820a66b16
luotao <luotaoyeah@gmail.com>
2018-12-28 20:58:43 +0800

    handbook: module: default export: import default export

--- src/handbook/13-modules/13-04-default-exports/13-04-01/C13040102.ts
+++ src/handbook/13-modules/13-04-default-exports/13-04-01/C13040102.ts
@@ -4,6 +4,11 @@
  */
 
 console.log("\n-------------------------------------------------- 01");
-import n from "./C13040101";
+/*
+ * 在语法上，引入 default export 和引入 named export 有些不同，
+ * 不需要使用 {}
+ * 且引入时的名称可以任意取
+ */
 
+import n from "./C13040101";
 console.log(n);
