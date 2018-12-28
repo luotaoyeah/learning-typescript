21a42d3600acf596903453a7fc81f71a37a877ea
luotao <luotaoyeah@gmail.com>
2018-12-28 21:14:02 +0800

    handbook: module: default export: default export a value

---
+++ src/handbook/13-modules/13-04-default-exports/13-04-01/C13040106.ts
@@ -0,0 +1,12 @@
+/*
+ * Modules
+ *     Default exports
+ *        default export a value
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * 可以直接默认输出一个字面量值
+ */
+
+export default "FOO";
