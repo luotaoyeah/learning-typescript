cef3f7f3b5a781e74be08f9e851a961e2f3aa941
luotao <luotaoyeah@gmail.com>
2019-01-05 10:45:11 +0800

    handbook: declaration merging: merging namespaces with enum

---
+++ src/handbook/17-declaration-merging/17-04-merging-namespaces-with-class/C170403.ts
@@ -0,0 +1,25 @@
+/*
+ * Declaration Merging
+ *     Merging Namespaces with Enum
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * namespace 和 enum 可以进行合并
+ * 通过这种方式，给 enum 对象添加静态属性和方法
+ */
+
+enum E {
+  X,
+  Y
+}
+
+namespace E {
+  export function fn01() {
+    console.log(E.X, E.Y);
+  }
+}
+
+E.fn01(); // 0 1
+
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./handbook/17-declaration-merging/17-04-merging-namespaces-with-class/C170402";
+import "./handbook/17-declaration-merging/17-04-merging-namespaces-with-class/C170403";
