67ad5a3208e7d4435a59b9e0ce565911a189b1a7
luotao <luotaoyeah@gmail.com>
2019-01-08 17:30:50 +0800

    handbook: decorator: accessor decorator

---
+++ src/handbook/19-decorators/19-08-accessor-decorator/C190801.ts
@@ -0,0 +1,38 @@
+/*
+ * Decorators
+ *     Accessor Decorator
+ */
+
+console.log("\n-------------------------------------------------- 01");
+
+/*
+ * accessor decorator 可以用在静态访问器和实例访问器上面
+ * accessor decorator 实际上装饰的是该 accessor 的 descriptor
+ */
+
+function upper(target: any, key: string, descriptor: PropertyDescriptor) {
+  if (descriptor.get) {
+    descriptor.get = new Proxy(descriptor.get, {
+      apply(target: () => any, thisArg: any, argArray?: any): any {
+        const value = target.apply(thisArg, argArray);
+
+        if (typeof value === "string") {
+          return value.toUpperCase();
+        }
+
+        return value;
+      }
+    });
+  }
+}
+
+class Person {
+  @upper
+  get name() {
+    return "tom";
+  }
+}
+
+console.log(new Person().name); // TOM
+
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./handbook/19-decorators/19-07-method-decorator/C190704";
+import "./handbook/19-decorators/19-08-accessor-decorator/C190801";
