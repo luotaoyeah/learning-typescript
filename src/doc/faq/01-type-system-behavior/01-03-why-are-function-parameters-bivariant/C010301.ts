8317448383355c11fc485d2a33c1cc8b59a61322
luotao <luotaoyeah@gmail.com>
2019-01-12 11:43:59 +0800

    fag: why-dont-i-get-type-checking-for-number--string-or-t--t

--- src/doc/faq/01-type-system-behavior/01-03-why-are-function-parameters-bivariant/C010301.ts
+++ src/doc/faq/01-type-system-behavior/01-03-why-are-function-parameters-bivariant/C010301.ts
@@ -20,7 +20,7 @@ class D extends A {
 function clone(a: A, callback: (a: A) => void): void {}
 
 /*
- * 问题如下：
+ * 问题描述：
  *     期望的是：clone() 方法的第一个参数和第二个回调函数的参数的类型一致，
  *     但是此处第一个参数类型是 C，第二个回调函数的参数类型是 D，为什么不报错？
  * 分析：
---
+++ src/doc/faq/01-type-system-behavior/01-11-why-dont-i-get-type-checking-for-number--string-or-t--t/C011101.ts
@@ -0,0 +1,29 @@
+/*
+ * Type System Behavior
+ *     Why don't I get type checking for (number) => string or (T) => T?
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 问题描述：
+   *     fn01() 的类型定义为：(number) => string，及参数为 number 类型，
+   *     为什么传递 boolean 类型的参数不会报错？
+   */
+
+  const fn01: (number) => string = n => n.toString();
+  fn01(true);
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * 问题分析：
+   *     TS 中定义函数类型时，对于参数的类型，
+   *     不能够只声明参数类型，而不声明参数名称，
+   *     所有上面的 (number) => stirng 中的 number 表示的是参数名称，而不是参数类型
+   *     它的类型是隐式的 any 类型，即相当于：(number: any) => string
+   */
+}
+
+export {};
