fc4e38fbcbae9c3b0002b20986f3a9075ad48fc3
luotao <luotaoyeah@gmail.com>
2019-01-12 11:32:21 +0800

    fag: why-doesnt-this-incorrect-cast-throw-a-runtime-error

---
+++ src/doc/faq/01-type-system-behavior/01-10-why-doesnt-this-incorrect-cast-throw-a-runtime-error/C011001.ts
@@ -0,0 +1,30 @@
+/*
+ * Type System Behavior
+ *     Why doesn't this incorrect cast throw a runtime error?
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 问题如下：
+   *     非法的类型转换为什么不会报错？
+   */
+
+  /*
+   * 为什么 true 转换为 string 不会报错？
+   */
+  let a: any = true;
+  let b: string = a as string;
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * 解决方法：
+   *     TS 中不存在类型转换（type conversion，type cast），
+   *     TS 中的类型断言（type assertion）只是告诉编译器说：
+   *         在编译期间，你就把它当成这种类型去对待
+   */
+}
+
+export {};
