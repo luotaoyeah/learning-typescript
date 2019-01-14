83cdcd0c4a8a5c8ced72cd9fd36cb0515e9e57aa
luotao <luotaoyeah@gmail.com>
2019-01-14 21:20:37 +0800

    fag: what-does-it-mean-for-an-interface-to-extend-a-class

---
+++ src/doc/faq/03-classes/03-05-what-does-it-mean-for-an-interface-to-extend-a-class/C030501.ts
@@ -0,0 +1,56 @@
+/*
+ * Classes
+ *     What does it mean for an interface to extend a class ?
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 问题描述：
+   *     当一个 interface extends 一个 class 时，发生了什么？
+   */
+
+  class A {
+    static x: string;
+    y: number;
+  }
+
+  interface B extends A {
+    z: number;
+  }
+
+  const b: B = { y: 9, z: 9 };
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * 问题分析：
+   *     当一个 interface extends 一个 class 时，
+   *     可以简单地理解为：把这个 class 当成一个 interface 在使用，
+   *     即：该 interface 继承了该 class 的实例类型（class 具有静态类型和实例类型）
+   */
+
+  class A {
+    constructor(y: number) {
+      this.y = y;
+    }
+
+    static x: number;
+    y: number;
+
+    static fn01() {}
+
+    fn02() {}
+  }
+
+  interface B extends A {}
+
+  /*
+   * 如下，B 继承了 A 的实例类型，因此实例属性 y 和实例方法 fn02() 都必须有，
+   * 而静态属性 x 和静态方法 fn01() 没有继承
+   */
+  const b: B = { y: 9, fn02() {} };
+}
+
+export {};
