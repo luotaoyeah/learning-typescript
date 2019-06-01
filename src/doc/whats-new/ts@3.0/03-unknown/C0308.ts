8168dc779c4729e337ea73c264d42f2c71f83d1f
luotao <luotaoyeah@gmail.com>
2019-06-01 14:36:38 +0800

    feat(ts@3.0): unknown: locals of type unknown always considered initialized

--- src/doc/whats-new/ts@3.0/03-unknown/C0308.ts
+++ src/doc/whats-new/ts@3.0/03-unknown/C0308.ts
@@ -2,38 +2,47 @@
  * https://github.com/Microsoft/TypeScript/wiki/What's-new-in-TypeScript#new-unknown-top-type
  *
  * New unknown Top Type
- *     initialization
+ *     Locals Of Type unknown Always Considered Initialized
  */
 
-console.log("\n-------------------------------------------------- 01");
+import chalk from "chalk";
+
+console.log(
+  chalk.red("\n-------------------------------------------------- 01")
+);
 {
   /*
    * 如果是 unknown 类型的变量, 可以不需要初始化, 直接赋值给其他变量
    */
 
+  // tslint:disable-next-line:prefer-const
   let x: unknown;
-  let y: any = x;
+  const y: any = x;
 
   /*
-   * 当开启了 strictPropertyInitialization 时,
-   * 如果 class 的 field 是 unknown 类型, 则不会报错
+   * 当开启了 --strictPropertyInitialization 时,
+   * 如果 class 的 field 是 unknown 类型, 即使它没有被初始化, 也不会报错,
+   * 而其他类型的 field 就会报错
    */
   class A {
-    // @ts-ignore
-    x: number; // TS2564: Property 'x' has no initializer and is not definitely assigned in the constructor.
-    y: unknown;
+    public x: unknown;
+    // @ts-ignore TS2564: Property 'x' has no initializer and is not definitely assigned in the constructor.
+    public y: number;
   }
 }
 
-console.log("\n-------------------------------------------------- 02");
+console.log(
+  chalk.green("\n-------------------------------------------------- 01")
+);
 {
   /*
    * 如果是其他类型的变量, 必须先进行初始化, 之后才能赋值给其他变量
    */
 
+  // tslint:disable-next-line:prefer-const
   let x: number;
-  // @ts-ignore
-  let y: any = x; // TS2454: Variable 'x' is used before being assigned.
+  // @ts-ignore TS2454: Variable 'x' is used before being assigned.
+  const y: any = x;
 }
 
 export {};
