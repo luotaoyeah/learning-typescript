14dae8c820473beae7826b3b727bcd11197a84dd
luotao <luotaoyeah@gmail.com>
2019-06-01 11:21:47 +0800

    feat(ts@3.5): higher order type inference from generic constructors

--- src/doc/whats-new/ts@2.9/support-number-and-symbol-named-properties-with-keyof-and-mapped-types.ts
+++ src/doc/whats-new/ts@2.9/support-number-and-symbol-named-properties-with-keyof-and-mapped-types.ts
@@ -1,9 +1,16 @@
-/**
+/*
  * https://github.com/Microsoft/TypeScript/wiki/What's-new-in-TypeScript#support-number-and-symbol-named-properties-with-keyof-and-mapped-types
+ *
+ * Support Number And Symbol Named Properties With Keyof And Mapped Types
  */
 
-console.log("\n-------------------------------------------------- 01");
+import chalk from "chalk";
 
-/* TODO */
+console.log(
+  chalk.red("\n-------------------------------------------------- 01")
+);
+{
+  /* TODO */
+}
 
 export {};
---
+++ src/doc/whats-new/ts@3.5/04-higher-order-type-inference-from-generic-constructors/C0401.ts
@@ -0,0 +1,16 @@
+/*
+ * https://github.com/Microsoft/TypeScript/wiki/What's-new-in-TypeScript#higher-order-type-inference-from-generic-constructors
+ *
+ * Higher Order Type Inference From Generic Constructors
+ */
+
+import chalk from "chalk";
+
+console.log(
+  chalk.red("\n-------------------------------------------------- 01")
+);
+{
+  /* TODO */
+}
+
+export {};
